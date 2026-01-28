61fee3d51f2554b73eab7030f683ddf51fc1cf28
Priyanshu272003 <priyansh272003@gmail.com>
2026-01-28 15:47:29 +0530

    arrays in python taking user input

--- array/import_array.py
+++ array/import_array.py
@@ -5,9 +5,20 @@ from array import *
 # for i in val:
 #     print(i, end = " ")
 
-arr = array('i', [10, 11, 12, 13, 15, 19])
+# arr = array('i', [10, 11, 12, 13, 15, 19])
+#
+# arr.insert(4, 14)
+# arr.append(20)
+# for i in range(0, len(arr)):
+#     print(arr[i], end = " ")
 
-arr.insert(4, 14)
-arr.append(20)
-for i in range(0, len(arr)):
-    print(arr[i], end = " ")
+n = int(input("Enter the number of elements you want: "))
+
+arr = array('i', [])
+
+for i in range(0, n):
+    arr.append(int(input("Enter the element: ")))
+
+print()
+for a in arr:
+    print(a, end=" ")
