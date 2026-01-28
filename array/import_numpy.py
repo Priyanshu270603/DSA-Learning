2c144489c1cd3c5b2099e485cd4e01f5077d346d
Priyanshu272003 <priyansh272003@gmail.com>
2026-01-28 22:09:29 +0530

    using numpy and performing searching

---
+++ array/import_numpy.py
@@ -0,0 +1,8 @@
+from numpy import *
+
+arr = array([12, 15, 13, 16, 18, 'p', 'k', 8.5])
+
+for i in arr:
+    print(i, end=" ")
+
+print(type(arr))
---
+++ array/searching.py
@@ -0,0 +1,7 @@
+from array import *
+
+arr = array('i', [15, 13, 231, 197, 85, 112, 119])
+
+i = arr.index(231)
+
+print(i)
